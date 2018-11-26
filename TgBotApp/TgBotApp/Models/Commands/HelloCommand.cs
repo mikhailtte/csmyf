3e68b3d6e1cbe4bc0f59859d1832a5facd75de32
mikhailtte <mikhail.tt.e@bk.ru>
2018-11-26 23:02:22 +0300

    Telegram Bot 1st public

---
+++ TgBotApp/TgBotApp/Models/Commands/HelloCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TgBotApp.Models.Commands
+{
+    public class HelloCommand : Command
+    {
+        public override string Name => "Hello";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+            //HERE CODE LOGIC
+
+
+
+            await client.SendTextMessageAsync(0, "", replyToMessageId: 0);
+        }
+    }
+}
---
+++ TgBotApp/TgBotApp/Models/TgBotSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TgBotApp.Models
+{
+    public class TgBotSettings
+    {
+        public static string Url { get; set; } = "https://da5dc909-0ee0-4-231-b9ee.azurewebsites.net/ ";
+
+        public static string Name { get; set; } = "lolboltbot";
+
+        public static string Key { get; set; } = "741222419:AAHyyzf0tBJynYxYkMCDPe0PR-L_ZBW9NrQ";
+    }
+}
