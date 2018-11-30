bdbddd0c0cfe085875865e1e81de4f609657017d
mikhailtte <mikhail.tt.e@bk.ru>
2018-11-30 22:28:00 +0300

    add proxy / proxy dll(empty)

---
+++ proxylib/proxylib/proxylib.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace proxylib
+{
+    public class proxylib : IWebProxy
+    {
+        public ICredentials Credentials
+        {
+            get { return new NetworkCredential("user", "password", "domain"); }
+            set { }
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri("http://my.proxy:8080");
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return false;
+        }
+
+    }
+}
