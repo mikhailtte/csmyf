4c3150dc7ef5d1b61ab7b761f26aa0dd18c05f1f
mikhailtte <mikhail.tt.e@bk.ru>
2018-12-01 18:02:57 +0300

    Create Beeper project, start rewrite by old code from BugFile/

---
+++ Beeper/Beeper/Models/ParseABCC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+
+namespace Beeper.Models
+{
+    public class ParseABCC
+    {
+        string GetHTML(string url)
+        {
+            
+        }
+        void ParseSellBuy(string htmlres)
+        {
+
+        }
+        void GetnParse(string url)
+        {
+            string htmlres = GetHTML(url);
+            ParseSellBuy(htmlres);
+            //присвоить статик переменным полученные значения(или вернуть массив с ними)
+        }
+    }
+}
---
+++ Beeper/Beeper/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Beeper")]
+[assembly: AssemblyDescription("Notification App of changes on the crypto terminal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VED")]
+[assembly: AssemblyProduct("Beeper")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("ee0a6f70-195d-4755-8d9a-0d43bc5f68e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
